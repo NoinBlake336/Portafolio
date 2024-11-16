e7bbf264a24a2a61be2a27415bc635d102c72082
NoinBlake336 <josechocobar336@gmail.com>
2024-11-16 16:54:37 -0300

    edit "project"

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -15,7 +15,7 @@ const Projects = () => {
         isTranslate
         ? (<>
           <Title 
-            title={'Projectos'} 
+            title={'Proyectos'} 
             icon={<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="icon">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M7 8l-4 4l4 4"></path>
